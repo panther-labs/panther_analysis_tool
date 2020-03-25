03562736b9a6dadf12c73d0683b7a044db84deb4
Nicholas Hakmiller <nicholas.hakmiller@runpanther.io>
2020-03-25 16:12:20 -0700

    updated pypi info

--- panther_analysis_tool/src/main.py
+++ panther_analysis_tool/src/main.py
@@ -358,7 +358,7 @@ def setup_parser() -> argparse.ArgumentParser:
         prog='panther_analysis_tool')
     parser.add_argument('--version',
                         action='version',
-                        version='panther_analysis_tool 0.1.9')
+                        version='panther_analysis_tool 0.1.10')
     subparsers = parser.add_subparsers()
 
     test_parser = subparsers.add_parser(
