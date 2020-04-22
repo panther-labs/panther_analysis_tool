7f434049a1dc0d956a5959ff44c806481cbde9c4
Nick <49166439+nhakmiller@users.noreply.github.com>
2020-04-22 13:58:58 -0700

    fixed some packaging issues (#21)

--- panther_analysis_tool/main.py
+++ panther_analysis_tool/main.py
@@ -415,7 +415,7 @@ def setup_parser() -> argparse.ArgumentParser:
         prog='panther_analysis_tool')
     parser.add_argument('--version',
                         action='version',
-                        version='panther_analysis_tool 0.1.10')
+                        version='panther_analysis_tool 0.2.1')
     subparsers = parser.add_subparsers()
 
     test_parser = subparsers.add_parser(
