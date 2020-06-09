a8e5a48d38f5190a1b1b01e71953b0c4bdafe20e
Nick <49166439+nhakmiller@users.noreply.github.com>
2020-06-08 18:29:30 -0700

    added better exception handling & filtering (#26)
    * added better exception handling & filtering
    
    * fixed linting issues
    
    * cleaned up logging
    
    * updated versions for new release

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from distutils.core import setup
 setup(
     name='panther_analysis_tool',
     packages=['panther_analysis_tool'],
-    version='0.3.0',
+    version='0.3.1',
     license='apache-2.0',
     description=
     'Panther command line interface for writing, testing, and packaging policies/rules.',
     author='Panther Labs Inc',
     author_email='pypi@runpanther.io',
     url='https://github.com/panther-labs/panther_analysis_tool',
-    download_url = 'https://github.com/panther-labs/panther_analysis_tool/archive/v0.3.0.tar.gz',
+    download_url = 'https://github.com/panther-labs/panther_analysis_tool/archive/v0.3.1.tar.gz',
     keywords=['Security', 'CLI'],
     scripts=['bin/panther_analysis_tool'],
     install_requires=[
