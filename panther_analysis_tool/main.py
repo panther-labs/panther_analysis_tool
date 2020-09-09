7b550221c59d5da68684eb9ebf89a36bb400d188
Nicholas Hakmiller <nicholas.hakmiller@runpanther.io>
2020-09-09 10:40:47 -0700

    version bump

--- panther_analysis_tool/main.py
+++ panther_analysis_tool/main.py
@@ -459,7 +459,7 @@ def setup_parser() -> argparse.ArgumentParser:
         prog='panther_analysis_tool')
     parser.add_argument('--version',
                         action='version',
-                        version='panther_analysis_tool 0.3.4')
+                        version='panther_analysis_tool 0.3.5')
     subparsers = parser.add_subparsers()
 
     test_parser = subparsers.add_parser(
