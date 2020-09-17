cb241d6cf093a30b3d5b9ee4a3fefb40ea24c168
Weyland <71197790+wey-chiang@users.noreply.github.com>
2020-09-17 18:38:05 -0400

    Removed unnecessary &

--- tests/unit/panther_analysis_tool/test_main.py
+++ tests/unit/panther_analysis_tool/test_main.py
@@ -70,7 +70,7 @@ class TestPantherAnalysisTool(TestCase):
         assert_equal(len(invalid_specs), 2)
 
     def test_invalid_characters(self):
-        args = pat.setup_parser().parse_args('test --path tests/fixtures --filter Severity=High PolicyID=AWS.IAM.MFAEnabled&'.split())
+        args = pat.setup_parser().parse_args('test --path tests/fixtures --filter Severity=High PolicyID=AWS.IAM.MFAEnabled'.split())
         args.filter = pat.parse_filter(args.filter)
         return_code, invalid_specs = pat.test_analysis(args)
         assert_equal(return_code, 1)
