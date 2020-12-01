4639ba20b565b2e5ca07c9d62c47a065971d8aca
Nick <49166439+nhakmiller@users.noreply.github.com>
2020-12-01 10:47:13 -0800

    0.4.0 release (#57)

--- panther_analysis_tool/main.py
+++ panther_analysis_tool/main.py
@@ -589,7 +589,7 @@ def setup_parser() -> argparse.ArgumentParser:
         prog='panther_analysis_tool')
     parser.add_argument('--version',
                         action='version',
-                        version='panther_analysis_tool 0.3.5')
+                        version='panther_analysis_tool 0.4.0')
     subparsers = parser.add_subparsers()
 
     test_parser = subparsers.add_parser(
