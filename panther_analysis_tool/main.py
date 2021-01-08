798aaf253703a2bded74c6cc933ab31a466db307
lindsey-w <lindsey.whitehurst@runpanther.io>
2021-01-08 09:42:00 -0800

    remove support for old format

--- panther_analysis_tool/main.py
+++ panther_analysis_tool/main.py
@@ -243,32 +243,7 @@ def upload_analysis(args: argparse.Namespace) -> Tuple[int, str]:
         response_str = response['Payload'].read().decode('utf-8')
         response_payload = json.loads(response_str)
 
-        if response_payload.get('errorMessage'):
-            # try deprecated version, using analysis-api gateway
-            deprecated_payload = {
-                'resource':
-                    '/upload',
-                'HTTPMethod':
-                    'POST',
-                'Body':
-                    json.dumps({
-                        'Data': base64.b64encode(zip_bytes).decode('utf-8'),
-                        # The UserID is required by Panther for this API call, but we have
-                        # no way of acquiring it and it isn't used for anything. This is a
-                        # valid UUID used by the Panther deployment tool to indicate this
-                        # action was performed automatically.
-                        'UserID': '00000000-0000-4000-8000-000000000000',
-                    }),
-            }
-            response = client.invoke(FunctionName='panther-analysis-api',
-                                     InvocationType='RequestResponse',
-                                     LogType='None',
-                                     Payload=json.dumps(deprecated_payload))
-            response_str = response['Payload'].read().decode('utf-8')
-            response_payload = json.loads(response_str)
-
-        if response_payload.get(
-                'errorMessage') or response_payload.get('statusCode') != 200:
+        if response_payload.get('statusCode') != 200:
             logging.warning(
                 'Failed to upload to Panther\n\tstatus code: %s\n\terror message: %s',
                 response_payload.get('statusCode', 0),
