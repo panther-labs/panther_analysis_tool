f4972ad92b07d41910b0f9c2892a05be4ab71f5c
Nicholas Hakmiller <nicholas.hakmiller@runpanther.io>
2021-03-22 14:46:18 -0700

    version bump

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from distutils.core import setup
 setup(
     name='panther_analysis_tool',
     packages=['panther_analysis_tool'],
-    version='0.5.0',
+    version='0.5.1',
     license='AGPL-3.0',
     description=
     'Panther command line interface for writing, testing, and packaging policies/rules.',
     author='Panther Labs Inc',
     author_email='pypi@runpanther.io',
     url='https://github.com/panther-labs/panther_analysis_tool',
-    download_url = 'https://github.com/panther-labs/panther_analysis_tool/archive/v0.5.0.tar.gz',
+    download_url = 'https://github.com/panther-labs/panther_analysis_tool/archive/v0.5.1.tar.gz',
     keywords=['Security', 'CLI'],
     scripts=['bin/panther_analysis_tool'],
     install_requires=[
