1539df1e5d90ddb4dd193ca6b77ebd51b5f06224
Nick <49166439+nhakmiller@users.noreply.github.com>
2021-03-22 15:06:14 -0700

    bump deps (#89)

--- panther_analysis_tool/main.py
+++ panther_analysis_tool/main.py
@@ -977,7 +977,7 @@ def setup_parser() -> argparse.ArgumentParser:
         + "managing Panther policies and rules.",
         prog="panther_analysis_tool",
     )
-    parser.add_argument("--version", action="version", version="panther_analysis_tool 0.5.1")
+    parser.add_argument("--version", action="version", version="panther_analysis_tool 0.5.2")
     parser.add_argument("--debug", action="store_true", dest="debug")
     subparsers = parser.add_subparsers()
 
--- setup.py
+++ setup.py
@@ -2,21 +2,23 @@ from distutils.core import setup
 setup(
     name='panther_analysis_tool',
     packages=['panther_analysis_tool'],
-    version='0.5.1',
+    version='0.5.2',
     license='AGPL-3.0',
     description=
     'Panther command line interface for writing, testing, and packaging policies/rules.',
     author='Panther Labs Inc',
     author_email='pypi@runpanther.io',
     url='https://github.com/panther-labs/panther_analysis_tool',
-    download_url = 'https://github.com/panther-labs/panther_analysis_tool/archive/v0.5.1.tar.gz',
+    download_url = 'https://github.com/panther-labs/panther_analysis_tool/archive/v0.5.2.tar.gz',
     keywords=['Security', 'CLI'],
     scripts=['bin/panther_analysis_tool'],
     install_requires=[
+        'boto3',
         'jsonpath-ng',
+        'requests',
         'ruamel.yaml',
         'schema',
-        'boto3',
+        'semver',
     ],
     classifiers=[
         'Development Status :: 4 - Beta',
