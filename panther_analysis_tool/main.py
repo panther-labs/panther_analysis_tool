ce205a0fae3a014c25f23975c6e69a31f79edca9
Weyland <71197790+wey-chiang@users.noreply.github.com>
2021-06-10 14:35:04 -0400

    v0.7.2 Version Bump (#106)
    * Version bump, added log_schemas to the manifest file
    
    * Bump to 0.7.2

--- panther_analysis_tool/main.py
+++ panther_analysis_tool/main.py
@@ -1267,7 +1267,7 @@ def setup_parser() -> argparse.ArgumentParser:
         + "managing Panther policies and rules.",
         prog="panther_analysis_tool",
     )
-    parser.add_argument("--version", action="version", version="panther_analysis_tool 0.7.0")
+    parser.add_argument("--version", action="version", version="panther_analysis_tool 0.7.2")
     parser.add_argument("--debug", action="store_true", dest="debug")
     subparsers = parser.add_subparsers()
 
