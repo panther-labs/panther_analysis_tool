ecc8862e54261c66b205f027da51b5fb015e0d43
George Psarakis <george.psarakis@runpanther.io>
2021-06-23 11:57:38 +0300

    Allow individual modules to be imported without loading main (#107)
    main module potentially performs global changes, such as changing the logging
    configuration that could possibly affect other code that imports
    individual standalone modules or sub-packages.

--- panther_analysis_tool/__init__.py
+++ panther_analysis_tool/__init__.py
@@ -16,5 +16,3 @@ GNU Affero General Public License for more details.
 You should have received a copy of the GNU Affero General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
-
-from panther_analysis_tool import main
