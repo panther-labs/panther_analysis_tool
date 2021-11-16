c968a9f91e44f7ac0db26b64296c39e4f26697b3
Weyland <71197790+wey-chiang@users.noreply.github.com>
2021-11-16 18:25:09 -0500

    Added parameters for unit testing the lastUpdatedVersion config

---
+++ tests/fixtures/detections/legacy_mocks/rules/example_legacy_rule_mocks_missing_version.py
@@ -0,0 +1,12 @@
+import boto3
+from datetime import date
+from unittest.mock import MagicMock
+
+def rule(event):
+    return all(isinstance(x, MagicMock) for x in [boto3, boto3.client, date])
+
+
+def title(event):
+    return f"BOTO3: {isinstance(boto3, MagicMock)} - " \
+           f"BOTO3.CLIENT: {isinstance(boto3.client, MagicMock)} - " \
+           f"DATE: {isinstance(date, MagicMock)}"
