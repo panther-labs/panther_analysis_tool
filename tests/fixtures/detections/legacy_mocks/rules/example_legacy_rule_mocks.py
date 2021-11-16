dd507340aa1012b349963a3c6041f175f9e4fc39
Weyland <71197790+wey-chiang@users.noreply.github.com>
2021-11-16 18:13:59 -0500

    Removed unused code from example legacy mocks

--- tests/fixtures/detections/legacy_mocks/rules/example_legacy_rule_mocks.py
+++ tests/fixtures/detections/legacy_mocks/rules/example_legacy_rule_mocks.py
@@ -2,9 +2,6 @@ import boto3
 from datetime import date
 from unittest.mock import MagicMock
 
-IGNORED_USERS = {}
-
-
 def rule(event):
     return all(isinstance(x, MagicMock) for x in [boto3, boto3.client, date])
 
