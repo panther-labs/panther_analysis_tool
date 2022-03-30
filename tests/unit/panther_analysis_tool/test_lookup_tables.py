a80ed63eb878643a75ef310938d23d977460e2e1
Matthew Cummings <matthew.cummings@runpanther.io>
2022-03-30 12:06:28 -0400

    test update

--- tests/unit/panther_analysis_tool/test_lookup_tables.py
+++ tests/unit/panther_analysis_tool/test_lookup_tables.py
@@ -30,7 +30,19 @@ class TestLookupTable(TestCase):  # pylint: disable=too-many-public-methods
     def test_load_invalid_specs_from_folder(self):
         args = argparse.Namespace()
         args.path = f'{LUTS_FIXTURES_PATH}/invalid/lookup-table-1.yml'
-        rc, file_path = pat.update_lookup_table(args)
+        rc, file_path = pat.test_lookup_table(args)
         self.assertEqual(1, rc)
+        self.assertEqual(file_path, "")
+
+    def test_load_invalid_specs_from_folder(self):
+        args = argparse.Namespace()
+        args.path = f'{LUTS_FIXTURES_PATH}/valid/lookup-table-1.yml'
+        rc, file_path = pat.test_lookup_table(args)
+        self.assertEqual(0, rc)
+        self.assertEqual(file_path, "")
+
+        args.path = f'{LUTS_FIXTURES_PATH}/valid/lookup-table-2.yml'
+        rc, file_path = pat.test_lookup_table(args)
+        self.assertEqual(0, rc)
         self.assertEqual(file_path, "")
 
