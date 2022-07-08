f5aade103aa307397db7142dc4151818c69cd4d6
Nic Martin <nicolas.martin@runpanther.io>
2022-07-08 11:56:26 -0500

    add connection check

--- panther_analysis_tool/backend/public_api_client.py
+++ panther_analysis_tool/backend/public_api_client.py
@@ -17,37 +17,78 @@ You should have received a copy of the GNU Affero General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
 
+import os
+import logging
+
+from gql import Client as GraphQLClient, gql
+from gql.transport.aiohttp import AIOHTTPTransport
+from typing import Dict, Union, Any
+from pathlib import Path
 from dataclasses import dataclass
-from gql import gql, Client
+
+from graphql import DocumentNode, ExecutionResult
 
 from .client import (
     Client,
     BackendResponse,
     BulkUploadParams,
+    BackendCheckResponse,
     ListDetectionsParams,
     DeleteDetectionsParams,
     ListSavedQueriesParams,
-    DeleteSavedQueriesParams, BackendCheckResponse
+    DeleteSavedQueriesParams,
 )
 
 
 @dataclass(frozen=True)
 class PublicAPIClientOptions:
-    host:    str
-    token:   str
+    host: str
+    token: str
     user_id: str
 
 
+class PublicAPIRequests:
+    _cache: Dict[str, str]
+
+    def __init__(self):
+        self._cache = dict()
+
+    def version_query(self) -> DocumentNode:
+        return self._load("get_version")
+
+    def _load(self, name: str) -> DocumentNode:
+        if name not in self._cache:
+            self._cache[name] = Path(_get_graphql_content_filepath(name)).read_text()
+
+        return gql(self._cache[name])
+
+
 class PublicAPIClient(Client):
-    _opts: PublicAPIClientOptions
+    _user_id: str
+    _requests: PublicAPIRequests
+    _gql_client: GraphQLClient
 
     def __init__(self, opts: PublicAPIClientOptions):
-        self._opts = opts
+        self._user_id = opts.user_id
+        self._requests = PublicAPIRequests()
+        self._gql_client = _build_client(opts.host, opts.token)
 
     def check(self) -> BackendCheckResponse:
+        res = self._execute(self._requests.version_query())
+
+        if res.errors:
+            for err in res.errors:
+                logging.error(err.message)
+
+            return BackendCheckResponse(
+                success=False,
+                message="connection check failed"
+            )
+
+        panther_version = res.data["generalSettings"]["pantherVersion"]
         return BackendCheckResponse(
-            success=False,
-            message="test"
+            success=True,
+            message=f"connected to Panther backend on version: {panther_version}"
         )
 
     def bulk_upload(self, params: BulkUploadParams) -> BackendResponse:
@@ -64,3 +105,29 @@ class PublicAPIClient(Client):
 
     def delete_detections(self, params: DeleteDetectionsParams) -> BackendResponse:
         pass
+
+    def _execute(self, request: DocumentNode) -> ExecutionResult:
+        return self._gql_client.execute(request, get_execution_result=True)
+
+
+_API_URL_PATH = "public/graphql"
+_API_DOMAIN_PREFIX = "api"
+_API_TOKEN_HEADER = "X-API-Key"
+
+
+def _build_client(host: str, token: str) -> GraphQLClient:
+    graphql_url = _build_api_url(host)
+    logging.info(f"Panther Public API endpoint: {graphql_url}")
+
+    transport = AIOHTTPTransport(url=graphql_url, headers={_API_TOKEN_HEADER: token})
+
+    return GraphQLClient(transport=transport, fetch_schema_from_transport=True)
+
+
+def _build_api_url(host: str):
+    return f"https://{_API_DOMAIN_PREFIX}.{host}/{_API_URL_PATH}"
+
+
+def _get_graphql_content_filepath(name: str):
+    wd = os.path.dirname(__file__)
+    return os.path.join(wd, "graphql", f"{name}.graphql")
