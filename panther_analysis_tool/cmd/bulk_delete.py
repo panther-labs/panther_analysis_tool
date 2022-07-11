cb356fe77e3caf59e793963fb10338df03cd2ee3
Nic Martin <nicolas.martin@runpanther.io>
2022-07-11 12:09:56 -0500

    impl bulk delete on hypo be response

--- panther_analysis_tool/cmd/bulk_delete.py
+++ panther_analysis_tool/cmd/bulk_delete.py
@@ -26,11 +26,72 @@ from panther_analysis_tool.backend.client import Client as BackendClient, BulkDe
 def run(backend: BackendClient, args: argparse.Namespace) -> Tuple[int, str]:
     logging.info("preparing bulk delete...")
 
+    # Get lists of analysis and queries from args
+    analysis_id_list = args.analysis_id
+    query_list = args.query_id
+
+    if len(analysis_id_list) == 0 and len(query_list) == 0:
+        logging.error("Must specify a list of analysis or queries to delete")
+        logging.error("Run panther_analysis_tool -h for help statement")
+        return 1, ""
+
     dry_run_res = backend.bulk_delete(BulkDeletePayload(dry_run=True, detection_ids=args.analysis_ids, saved_query_ids=args.query_ids))
+    if dry_run_res.status_code != 200:
+        logging.error("Error connecting to backend for delete.")
+        return 1, ""
 
-    # TODO: introspect on dry run metadata, prompt as necessary...
-    # potentially expand the scope of the delete...
+    dr_info = dry_run_res.data["dryRun"]
 
-    res = backend.bulk_delete(BulkDeletePayload(dry_run=False, detection_ids=args.analysis_ids, saved_query_ids=args.query_ids))
+    for not_found_detection_id in dr_info["detectionIdsNotFound"]:
+        logging.info(f"Detection '{not_found_detection_id}' was not found.")
+
+    for not_found_query_id in dr_info["savedQueryIdsNotFound"]:
+        logging.info(f"Saved Query '{not_found_query_id}' was not found.")
+
+    # BE needs to:
+    # - expand saved detection id list to include scheduled rules matching saved query ids.
+    # - validate which detection ids actually exist (metadata.detectionIdsNotFound)
+
+    # {
+    #     "detectionIds": [],  // empty because it's a dry run
+    #     "savedQueryIds": [], // empty because it's a dry run
+    #     "dryRun": {
+    #         "stats": {
+    #           "totalDetectionsToDelete": 0,
+    #           "totalSavedQueriesToDelete": 0,
+    #         }
+    #
+    #         "detectionIdsToDelete":  [],
+    #         "detectionIdsNotFound":  [],
+    #         "savedQueryIdsToDelete": [],
+    #         "savedQueryIdsNotFound": [],
+    #
+    #         "linkedDetectionsToDelete":   [],
+    #         "linkedSavedQueriesToDelete": [],
+    #     }
+    # }
+
+    # Unless explicitly bypassed, get user confirmation to delete
+    if not args.confirm_bypass:
+        dr_stats = dr_info["stats"]
+        total_detections = dr_stats["totalDetectionsToDelete"]
+        total_saved_queries = dr_stats["totalSavedQueriesToDelete"]
+
+        if total_detections > 0:
+            logging.warning("You are about to delete detections %s", total_detections)
+        if total_saved_queries > 0:
+            logging.warning("You are about to delete saved queries %s", total_saved_queries)
+
+        confirm = input("Continue? (y/n) ")
+
+        if confirm.lower() != "y":
+            print("Cancelled")
+            return 0, ""
+
+    delete_res = backend.bulk_delete(BulkDeletePayload(dry_run=False, detection_ids=args.analysis_ids, saved_query_ids=args.query_ids))
+
+    if delete_res.status_code != 200:
+        logging.error("Error connecting to backend for delete.")
+        return 1, ""
 
     return 0, ""
