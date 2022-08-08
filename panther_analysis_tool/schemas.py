ea97b828276634389bfe187bc21a7dba0434e2d5
Daria <daria.artcykhovich@panther.io>
2022-08-08 16:09:18 -0700

    Update Lookup Table schema with a new field

--- panther_analysis_tool/schemas.py
+++ panther_analysis_tool/schemas.py
@@ -204,7 +204,7 @@ LOOKUP_TABLE_SCHEMA = Schema(
         "AnalysisType": Or("lookup_table"),
         "LookupName": str,
         "Enabled": bool,
-        Or("DataFile", "Refresh"): Or(
+        Or("FileName", "Refresh"): Or(
             str,
             {
                 "RoleARN": str,
