4a8627ca89ba301e2e6d61cc287216f00df8aca4
Dan Biwer <dan.biwer@runpanther.io>
2022-10-11 13:23:44 -0500

    Danbiwer inc version 16 (#216)
    * inc version to 16
    
    * inc version

--- panther_analysis_tool/main.py
+++ panther_analysis_tool/main.py
@@ -1479,7 +1479,7 @@ def setup_parser() -> argparse.ArgumentParser:
                     + "managing Panther policies and rules.",
         prog="panther_analysis_tool",
     )
-    parser.add_argument("--version", action="version", version="panther_analysis_tool 0.15.1")
+    parser.add_argument("--version", action="version", version="panther_analysis_tool 0.16.0")
     parser.add_argument("--debug", action="store_true", dest="debug")
     subparsers = parser.add_subparsers()
 
