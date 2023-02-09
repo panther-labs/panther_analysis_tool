068420c2ed8ef6555fe1e92714454e96d9b8fc5a
Max Richmond <46904505+maxrichie5@users.noreply.github.com>
2023-02-09 08:36:56 -0700

    updating doc comment for to_list

--- panther_analysis_tool/util.py
+++ panther_analysis_tool/util.py
@@ -138,4 +138,5 @@ def deep_get(obj: Dict, path: List[str], default: Any = None) -> Any:
 
 
 def to_list(listish: Any) -> List:
+    """Make a single instance a list or keep a list a list."""
     return listish if isinstance(listish, list) else [listish]
