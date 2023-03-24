ed4e72138ad68a4cd7b556c4a033633620e18955
lindsey-w <lindsey.whitehurst@panther.com>
2023-03-24 11:26:37 -0700

    add unit test

--- tests/unit/panther_analysis_tool/test_schemas.py
+++ tests/unit/panther_analysis_tool/test_schemas.py
@@ -2,7 +2,7 @@ import unittest
 
 from schema import SchemaError
 
-from panther_analysis_tool.schemas import LOG_TYPE_REGEX
+from panther_analysis_tool.schemas import LOG_TYPE_REGEX, MOCK_SCHEMA
 
 
 class TestPATSchemas(unittest.TestCase):
@@ -12,3 +12,10 @@ class TestPATSchemas(unittest.TestCase):
 
         with self.assertRaises(SchemaError):
             LOG_TYPE_REGEX.validate("Amazon.EKS.Foo")
+
+    def test_mocks_are_str(self):
+        MOCK_SCHEMA.validate({"objectName": "hello", "returnValue": "Testing a string"})
+        MOCK_SCHEMA.validate({"objectName": "hello", "returnValue": "False"})
+
+        with self.assertRaises(SchemaError):
+            LOG_TYPE_REGEX.validate({"objectName": "hello", "returnValue": ["Testing a string"]})
