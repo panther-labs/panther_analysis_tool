1e52b5ece46cd7bc6d410736bc7afc0fe02daa39
lindsey-w <lindsey.whitehurst@panther.com>
2023-04-06 12:13:38 -0700

    nit

--- panther_analysis_tool/util.py
+++ panther_analysis_tool/util.py
@@ -62,9 +62,9 @@ def get_latest_version() -> str:
     return UNKNOWN_VERSION
 
 
-def is_latest(test_version: str) -> bool:
+def is_latest(latest_version: str) -> bool:
     try:
-        return version.parse(VERSION_STRING) >= version.parse(test_version)
+        return version.parse(VERSION_STRING) >= version.parse(latest_version)
     except Exception:  # pylint: disable=broad-except
         logging.debug("Unable to determine latest version", exc_info=True)
     # if we run into any issues connecting or parsing the version,
