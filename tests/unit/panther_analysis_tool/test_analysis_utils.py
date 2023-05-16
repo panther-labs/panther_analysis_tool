0f32c287952026c4a22d433e47065d93dafeb6b3
lindsey-w <lindsey.whitehurst@panther.com>
2023-05-16 08:15:42 -0700

    chkpt

---
+++ tests/unit/panther_analysis_tool/test_analysis_utils.py
@@ -0,0 +1,87 @@
+"""
+Panther Analysis Tool is a command line interface for writing,
+testing, and packaging policies/rules.
+Copyright (C) 2020 Panther Labs Inc
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as
+published by the Free Software Foundation, either version 3 of the
+License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
+from unittest import mock, TestCase
+
+
+from panther_analysis_tool.analysis_utils import get_simple_detections_as_python
+from panther_analysis_tool.backend.client import BackendResponse, BackendError, TranspileToPythonResponse
+from panther_analysis_tool.backend.mocks import MockBackend
+
+class TestAnalysisUtils(TestCase):
+
+    def get_specs_for_test(self):
+        specs = []
+        for i in range(10):
+            specs.append(
+                (
+                    f"filname-{i}",
+                    f"filepath-{i}",
+                    {
+                        "Detection": [
+                            {
+                                "Key": "event_type",
+                                "Condition": "Equals",
+                                "Value": f"team_privacy_settings_changed-{i}"
+                            },
+                            {
+                                "DeepKey": ["details", "new_value"],
+                                "Condition": "Equals",
+                                "Value": f"public-{i}"
+                            }
+                        ]
+                    },
+                )
+            )
+        return specs
+
+    def test_no_backend(self) -> None:
+        specs = self.get_specs_for_test()
+        self.assertEqual(get_simple_detections_as_python(None, specs), specs)
+
+    def test_backend_error(self) -> None:
+        specs = self.get_specs_for_test()
+        backend = MockBackend()
+        backend.transpile_simple_detection_to_python = mock.MagicMock(
+            side_effect=BackendError("that won't transpile!")
+        )
+        self.assertEqual(get_simple_detections_as_python(backend, specs), specs)
+
+    def test_base_error(self) -> None:
+        specs = self.get_specs_for_test()
+        backend = MockBackend()
+        backend.transpile_simple_detection_to_python = mock.MagicMock(
+            side_effect=BaseException("uh-oh")
+        )
+        self.assertEqual(get_simple_detections_as_python(backend, specs), specs)
+
+    def test_happy_path(self) -> None:
+        specs = self.get_specs_for_test()
+        backend = MockBackend()
+        backend.transpile_simple_detection_to_python = mock.MagicMock(
+            return_value=BackendResponse(
+                data=TranspileToPythonResponse(
+                    transpiledPython=["def rule(event): return True" for _ in range(len(specs))],
+                ),
+                status_code=200,
+            )
+        )
+        output = get_simple_detections_as_python(backend, specs)
+        for actual in output:
+            self.assertEqual(actual[2]["body"], "def rule(event): return True")
