3fc726a8ff5f816763e1c87f6b34bdeb01c7543a
Darwayne <darwaynelynch@gmail.com>
2023-06-06 14:30:22 -0400

    bump verison to 0.22.3

--- panther_analysis_tool/constants.py
+++ panther_analysis_tool/constants.py
@@ -16,7 +16,7 @@ from panther_analysis_tool.schemas import (
 
 PACKAGE_NAME: Final = "panther_analysis_tool"
 
-VERSION_STRING: Final = "0.22.2"
+VERSION_STRING: Final = "0.22.3"
 
 CONFIG_FILE = ".panther_settings.yml"
 DATA_MODEL_LOCATION = "./data_models"
