93da15ca7c9c01f5bc059e1f35a976c92782275d
George Papanikolaou <george.papanikolaou@panther.io>
2023-06-08 11:33:32 +0300

    Add character limit check for schema names (#299)
    * Add character limit check for schema names
    
    * Automatically commit format changes
    
    * Update panther_analysis_tool/log_schemas/user_defined.py
    
    Co-authored-by: darwayne <darwaynelynch@gmail.com>
    
    ---------
    
    Co-authored-by: papanikge <papanikge@users.noreply.github.com>
    Co-authored-by: darwayne <darwaynelynch@gmail.com>

--- panther_analysis_tool/log_schemas/user_defined.py
+++ panther_analysis_tool/log_schemas/user_defined.py
@@ -203,6 +203,9 @@ class Uploader:
                 f" with the prefix '{self._SCHEMA_NAME_PREFIX}'",
             )
 
+        if len(name) > 255:
+            return ("", "'schema' field: value should not exceed 255 characters")
+
         return name, None
 
     def _update_or_create_schema(
