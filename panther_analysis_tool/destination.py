658bcf5eae347030095ac152f1f2b79db180b9ae
Mykola Demianchuk <mykola.demianchuk@panther.io>
2023-07-10 18:06:28 -0500

    More testing

--- panther_analysis_tool/destination.py
+++ panther_analysis_tool/destination.py
@@ -1,10 +1,8 @@
 from dataclasses import dataclass
 
-
 @dataclass
 class FakeDestination:
     """Stub class as a replacement for the Destination class
     that wraps alert output metadata."""
-
     destination_id: str
     destination_display_name: str
