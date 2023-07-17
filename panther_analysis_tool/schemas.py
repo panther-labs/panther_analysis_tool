e710876750fc573a6c2dda8b0d4184c04b5b83bd
Grant Joy <9968195+grantjoy@users.noreply.github.com>
2023-07-17 16:09:08 -0700

    Update schemas.py (#322)

--- panther_analysis_tool/schemas.py
+++ panther_analysis_tool/schemas.py
@@ -55,6 +55,7 @@ LOG_TYPE_REGEX = Regex(
     r"^(Apache\.AccessCombined|Amazon\.EKS\.Audit|Amazon\.EKS\.Authenticator|Apache\.AccessCommon"
     r"|Asana\.Audit|Atlassian\.Audit|AWS\.ALB|AWS\.AuroraMySQLAudit"
     r"|Auth0\.Events"
+    r"|Linux\.Auditd"
     r"|Netskope\.Audit"
     r"|AWS\.CloudTrail|AWS\.CloudTrailDigest|AWS\.CloudTrailInsight|AWS\.CloudWatchEvents"
     r"|AWS\.GuardDuty|AWS\.S3ServerAccess|AWS\.VPCDns|AWS\.VPCFlow|AWS\.WAFWebACL|Box\.Event"
