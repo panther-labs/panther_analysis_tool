238f5410eb5b80ae255f8747fff3cfa5b7408fe0
panther-bot-automation <94577522+panther-bot-automation@users.noreply.github.com>
2023-11-09 13:47:13 -0600

    Bump version to 0.32.1 (#409)

--- panther_analysis_tool/constants.py
+++ panther_analysis_tool/constants.py
@@ -18,7 +18,7 @@ from panther_analysis_tool.schemas import (
 
 PACKAGE_NAME: Final = "panther_analysis_tool"
 
-VERSION_STRING: Final = "0.32.0"
+VERSION_STRING: Final = "0.32.1"
 
 CONFIG_FILE = ".panther_settings.yml"
 DATA_MODEL_LOCATION = "./data_models"
