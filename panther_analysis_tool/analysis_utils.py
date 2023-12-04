7415b3680d45218b2732ab56867187be507eb20e
Nikita Skobov <nikita.skobov@panther.io>
2023-12-04 17:34:28 -0600

    formatting

--- panther_analysis_tool/analysis_utils.py
+++ panther_analysis_tool/analysis_utils.py
@@ -184,12 +184,13 @@ def load_analysis_specs(
     for result in load_analysis_specs_ex(directories, ignore_files, roundtrip_yaml=False):
         yield result.spec_filename, result.relative_path, result.analysis_spec, result.error
 
+
 def disable_all_base_detections(paths: List[str], ignore_files: List[str]) -> None:
     analysis_specs = list(load_analysis_specs_ex(paths, ignore_files, roundtrip_yaml=True))
     base_ids_to_disable = set()
-    base_detection_key = 'BaseDetection'
-    rule_id_key = 'RuleID'
-    enabled_key = 'Enabled'
+    base_detection_key = "BaseDetection"
+    rule_id_key = "RuleID"
+    enabled_key = "Enabled"
     for analysis_spec_res in analysis_specs:
         spec: Dict[str, Any] = analysis_spec_res.analysis_spec
         base_id = spec.get(base_detection_key, "")
@@ -203,6 +204,7 @@ def disable_all_base_detections(paths: List[str], ignore_files: List[str]) -> No
                 logging.info(f"Setting {enabled_key}=False for {analysis_spec_res.spec_filename}")
                 rule[enabled_key] = False
                 analysis_spec_res.serialize_to_file()
+
 
 @dataclasses.dataclass
 class LoadAnalysisSpecsResult:
