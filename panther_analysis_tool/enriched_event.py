e90b34073c6dbc32b903a1b90636de60e2958d99
Darwayne <darwaynelynch@gmail.com>
2024-05-31 11:15:09 -0400

    fixup.

--- panther_analysis_tool/enriched_event.py
+++ panther_analysis_tool/enriched_event.py
@@ -17,8 +17,7 @@ You should have received a copy of the GNU Affero General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 
-from panther_core.enriched_event import PantherEvent as PantherCoreEvent
+from panther_core.enriched_event import PantherEvent
 
+__all__ = ['PantherEvent']
 
-class PantherEvent(PantherCoreEvent):
-    pass
