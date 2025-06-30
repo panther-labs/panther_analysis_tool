494e2f3cf3c3e0592ef4e5c2742b0fc50febf0b8
Ben Airey <ben.airey@panther.com>
2025-06-30 17:46:31 -0500

    finally fixed

--- panther_analysis_tool/validation.py
+++ panther_analysis_tool/validation.py
@@ -4,11 +4,14 @@ from typing import Any, List, Optional
 
 from nested_lookup import nested_lookup
 from sqlfluff import parse
+from sqlfluff.core.config import FluffConfig
 
 from panther_analysis_tool.analysis_utils import ClassifiedAnalysisContainer
 from panther_analysis_tool.constants import SET_FIELDS
 
 # This file was generated in whole or in part by GitHub Copilot.
+
+SQLFLUFF_COMFIG = FluffConfig(overrides={"dialect": "snowflake", "templater": "jinja"})
 
 
 def contains_invalid_field_set(analysis_spec: Any) -> List[str]:
@@ -35,9 +38,10 @@ def contains_invalid_table_names(
 ) -> List[str]:
     invalid_table_names = []
     query = lookup_snowflake_query(analysis_spec)
+    # breakpoint()
     if query is not None:
         try:
-            parsed_query = parse(query, "snowflake")
+            parsed_query = parse(query, config=SQLFLUFF_COMFIG)
         except Exception as exc:  # pylint: disable=broad-except
             # Intentionally broad exception catch:
             # We want to fall back on original behavior if this third-party parser cannot tell us the table names
@@ -73,6 +77,7 @@ def contains_invalid_table_names(
                         invalid_table_names.append(table_name)
     else:
         logging.info("No query found for %s", analysis_id)
+    # breakpoint()
     return invalid_table_names
 
 
